eb3a75d16b8cf267638fe538fde80c73bb52775c
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 16:54:06 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 import { initBinder } from "./binder.js";
 import { initHeader } from "./renderers/headerRenderer.js";
+import { DATA_SOURCES } from "./config/dataSources.js";
+import { fetchCSV } from "./core/dataFetcher.js";
+import { setRawData } from "./core/stateManager.js";
+import { startProgress, finishProgress } from "./engines/progress/progressEngine.js";
+import { initFilters } from "./engines/filters/filterEngine.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-
-    console.log("App Booting...");
+document.addEventListener("DOMContentLoaded", async () => {
 
     initHeader();
     initBinder();
+    initFilters();
+
+    startProgress();
+
+    const rawData = {};
+
+    for (const key in DATA_SOURCES) {
+        rawData[key] = await fetchCSV(DATA_SOURCES[key]);
+    }
+
+    setRawData(rawData);
+
+    finishProgress();
 
 });
