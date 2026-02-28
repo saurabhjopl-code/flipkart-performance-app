65bafb251c2580101bbf84e0d6fda0efd61850b5
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 16:49:04 +0530

    Update dataSources.js

---
+++ js/config/dataSources.js
@@ -0,0 +1,17 @@
+export const DATA_SOURCES = {
+
+    // GMV
+    GMV_DATE: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS5f6lc5gca6OccttwEikVrrnarFg1nYXdOUhtE4P04kZxIlrALGewx53wfzEqahNgXS6muKbtxQt8b/pub?gid=378965185&single=true&output=csv",
+    GMV_SKU: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS5f6lc5gca6OccttwEikVrrnarFg1nYXdOUhtE4P04kZxIlrALGewx53wfzEqahNgXS6muKbtxQt8b/pub?gid=485165078&single=true&output=csv",
+    GMV_FULFILLMENT: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS5f6lc5gca6OccttwEikVrrnarFg1nYXdOUhtE4P04kZxIlrALGewx53wfzEqahNgXS6muKbtxQt8b/pub?gid=241043414&single=true&output=csv",
+    GMV_VERTICAL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS5f6lc5gca6OccttwEikVrrnarFg1nYXdOUhtE4P04kZxIlrALGewx53wfzEqahNgXS6muKbtxQt8b/pub?gid=1993907418&single=true&output=csv",
+
+    // ADS
+    ADS_DATE: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC6Kur52aUkBu312BfFcbeyojUKEEBQTUspMec3_WuZ68ryNGmphWfATST-qBQ6g9Hq9mwThjacBTk/pub?gid=1189847201&single=true&output=csv",
+    ADS_CAMPAIGN: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSC6Kur52aUkBu312BfFcbeyojUKEEBQTUspMec3_WuZ68ryNGmphWfATST-qBQ6g9Hq9mwThjacBTk/pub?gid=1872878980&single=true&output=csv",
+
+    // TRAFFIC
+    TRAFFIC_DATE: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGHP-uZXmzgZihLGZu6UKcQPV-VotSqlYGUzWYifknjhcCG1SEz-P97K5SpCt6TFrD7Ce97V6xcTtT/pub?gid=371070730&single=true&output=csv",
+    TRAFFIC_SKU: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGHP-uZXmzgZihLGZu6UKcQPV-VotSqlYGUzWYifknjhcCG1SEz-P97K5SpCt6TFrD7Ce97V6xcTtT/pub?gid=219569921&single=true&output=csv",
+    TRAFFIC_VERTICAL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGHP-uZXmzgZihLGZu6UKcQPV-VotSqlYGUzWYifknjhcCG1SEz-P97K5SpCt6TFrD7Ce97V6xcTtT/pub?gid=1761473304&single=true&output=csv"
+};
